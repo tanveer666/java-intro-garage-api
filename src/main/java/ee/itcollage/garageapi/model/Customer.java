f46a331116b2201c16d31b56e670a275088bad97
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-03-15 20:56:19 +0200

    Remove lombok

--- src/main/java/ee/itcollage/garageapi/model/Customer.java
+++ src/main/java/ee/itcollage/garageapi/model/Customer.java
@@ -1,16 +1,11 @@
 package ee.itcollage.garageapi.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
 public class Customer {
 
     @Id
@@ -23,6 +18,30 @@ public class Customer {
 
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
