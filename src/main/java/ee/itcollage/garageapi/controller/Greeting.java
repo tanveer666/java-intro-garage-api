e274b04824f6d8a15ef731574655cdb320e19f84
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-03-25 22:32:35 +0200

    Master solution

--- src/main/java/ee/itcollage/garageapi/controller/Greeting.java
+++ src/main/java/ee/itcollage/garageapi/controller/Greeting.java
@@ -2,6 +2,7 @@ package ee.itcollage.garageapi.controller;
 
 public class Greeting {
     private String name;
+    private Integer order;
 
     public Greeting() {
     }
@@ -10,11 +11,24 @@ public class Greeting {
         this.name = name;
     }
 
+    public Greeting(String name, Integer order) {
+        this.name = name;
+        this.order = order;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
     }
 }
--- src/main/java/ee/itcollage/garageapi/controller/GreetingController.java
+++ src/main/java/ee/itcollage/garageapi/controller/GreetingController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static java.lang.String.format;
 
 
@@ -28,11 +31,21 @@ public class GreetingController {
     }
 
     //todo
-    // * do you understand what's going on?
-    // * install postman and try to access POST method
-    // * create GET method that returns numbers
-    // * create GET method that returns a list of greetings
-    // * add more info to Greeting object
+    // create GET method that returns numbers
+    @GetMapping("numbers")
+    public List<Integer> numbers() {
+        return Arrays.asList(1, 2, 3);
+    }
+
+    //todo
+    // create GET method that returns a list of greetings
+    @GetMapping("list")
+    public List<Greeting> greetings() {
+        return Arrays.asList(new Greeting("We are legion!", 1), new Greeting("We are bob!", 2));
+    }
+
+    //todo
     // * create an example of @PathVariable
     // * test out PUT, DELETE or PATCH method
+    // garage-cars/garage-cars-solution branches
 }
