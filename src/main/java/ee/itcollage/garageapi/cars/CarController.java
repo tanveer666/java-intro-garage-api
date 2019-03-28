8b74fa48621fa9df8b8f64a2618d51fe760e5c5b
mhasan <mhasan@ttu.ee>
2019-03-28 14:27:24 +0200

    Added functionality to get,add,replace and update 1 car

--- src/main/java/ee/itcollage/garageapi/cars/CarController.java
+++ src/main/java/ee/itcollage/garageapi/cars/CarController.java
@@ -2,29 +2,68 @@
 package ee.itcollage.garageapi.cars;
 
 import com.google.common.collect.Lists;
-import ee.itcollage.garageapi.controller.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.String.format;
 
 
 @RestController
-@RequestMapping("/cars")
+
+@RequestMapping("/cars") //class level controller, maps all requests made on the URL cars to this class (and methods)
 public class CarController {
 
     private List<Car> cars = Lists.newArrayList(new Car(1L, "Jaguar"), new Car(2L, "Mercedes"));
 
-    @GetMapping
+    @GetMapping //maps to all get requests on /cars only
     public List<Car> getCars() {
         return cars;
     }
+
+
+    @GetMapping("/{id}") //maps all get requests on /cars/int to this
+    public Car carGet(@PathVariable int id) {
+
+        List<Car> get = cars.stream()
+                .filter(car -> car.getId().intValue() == id)
+                .collect(Collectors.toList());
+
+        if (get.isEmpty()) {
+            return new Car(0L, format("no such car exists by the id of %d", id));
+        } else {
+            return get.get(0);
+        }
+
+    }
+
+    @PostMapping()
+    public void carPost(@RequestBody Car newCar) {
+        cars.add(newCar);
+    }
+
+    @PutMapping
+    public void carPut(@RequestBody Car updateCar) {
+        cars = cars.stream()
+                .filter(car -> car.getId().intValue() != updateCar.getId().intValue())
+                .collect(Collectors.toList());
+
+        cars.add(updateCar);
+    }
+
+    @DeleteMapping
+    public void carDel(@RequestBody Car deleteCar) {
+        cars = cars.stream()
+                .filter(car -> car.getId().intValue() != deleteCar.getId().intValue())
+                .collect(Collectors.toList());
+
+        //cars.remove(deleteCar);
+
+    }
+
 
     //todo
     // get 1 car
